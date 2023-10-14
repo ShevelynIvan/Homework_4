ee2e0d5fdfa694cd2ed0310277c720b874ab3a38
ShevelynIvan <ivan.shevelyn1@gmail.com>
2023-10-14 14:19:55 +0300

    Добавьте файлы проекта.

---
+++ Points/Point.cs
@@ -0,0 +1,57 @@
+﻿namespace Points
+{
+    internal class Point
+    {
+        /// <summary>
+        /// Static field shows number of created points. 
+        /// </summary>
+        public static int CountOfObjects { get; private set; }
+
+        /// <summary>
+        /// X coordinate of the point
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Y coordinate of the point
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Creates a point (0;0)
+        /// </summary>
+        public Point() 
+        {
+            CountOfObjects++;
+        }
+
+        /// <summary>
+        /// Creates a point
+        /// </summary>
+        /// <param name="x">X coordinate of the point</param>
+        /// <param name="y">Y coordinate of the point</param>
+        public Point(double x, double y)
+        {
+            CountOfObjects++;
+            X = x; 
+            Y = y;
+        }
+
+        /// <summary>
+        /// Calculates the distance between two points
+        /// </summary>
+        /// <param name="a">First point 'A'</param>
+        /// <param name="b">Second point 'B'</param>
+        /// <returns>Rounded integer value of distance between points</returns>
+        public static int GetDistanceBetweenPoints(Point a, Point b) 
+            => (int)Math.Sqrt(Math.Pow((a.X - b.X), 2) + Math.Pow((a.Y - b.Y), 2));
+
+        /// <summary>
+        /// Calculates the distance to point from this point
+        /// </summary>
+        /// <param name="a">The point to which we calculate the distance</param>
+        /// <returns>Rounded integer value of distance from this point to point 'A'</returns>
+        public int GetDistanceToPoint(Point a)
+            => (int)Math.Sqrt(Math.Pow((a.X - X), 2) + Math.Pow((a.Y - Y), 2));
+    }
+}
---
+++ Points/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Points
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point() { X = 0.95, Y = 16.4 };
+            Point p2 = new Point() { X = -5.5, Y = 1.31 };
+            Point p3 = new Point(1, 1);
+            Console.WriteLine($"Count of created points = {Point.CountOfObjects}");
+            Console.WriteLine($"Distance from p1 to p2 = {Point.GetDistanceBetweenPoints(p1, p2)}");
+            Console.WriteLine($"Distance from p2 to p1 = {p2.GetDistanceToPoint(p1)}");
+            Console.WriteLine($"Distance from p2 to p3 = {p2.GetDistanceToPoint(p3)}");
+            Console.WriteLine($"Distance from p1 to p3 = {Point.GetDistanceBetweenPoints(p1, p3)}");
+        }
+    }
+}
